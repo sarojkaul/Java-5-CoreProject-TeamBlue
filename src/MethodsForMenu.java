076ee0ae3ef39ce9804cc65e26f8bc44fe0b8b41
saroj kaul <sarojmahi73@gmail.com>
2020-12-15 15:57:43 +0100

    adding some mehods file

--- src/MethodsForMenu.java
+++ src/MethodsForMenu.java
@@ -90,24 +90,25 @@ public class MethodsForMenu {
         public void cancellation () {
 
                 System.out.println("****** Cancel Booking ********");
-            try (Connection connection = DriverManager.getConnection(databaseUrl, connectionProperties))
-            {
-                Statement statement = connection.createStatement();
+
                 String sql;
                 // Query for display all students
                 sql = "INSERT INTO `cancellation`(`Cancellation_ID`, `Cancellation_Date`, `Booking_ID`) VALUES(?,?,?)";
 
                 Scanner scanner = new Scanner(System.in);
-                System.out.println("Enter Cancellation Id: ");
-                int cancellation_Id = scanner.nextInt();
-                System.out.println("Enter cancellation date: ");
+            System.out.println("Enter cancelation_ID: " );
+            int id = scanner.nextInt();
+            scanner.nextLine();
+                System.out.println("Enter cancellation date(\"MM/dd/yyyy\") : ");
                String cancellation_date = scanner.nextLine();
                 LocalDate Cancellation_date = LocalDate.parse(cancellation_date, DATE_INPUT_FORMAT);
                 System.out.println(Cancellation_date);
                 System.out.println("Enter Booking_ID want to cancel: ");
                 int booking_id = scanner.nextInt();
-                PreparedStatement preparedStatement = connection.prepareStatement(sql);
-                preparedStatement.setInt(1, cancellation_Id);
+            try (Connection connection = DriverManager.getConnection(databaseUrl, connectionProperties);
+                 PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS))
+            {
+                preparedStatement.setInt(1,id);
                preparedStatement.setDate(2, Date.valueOf(cancellation_date));
                 preparedStatement.setInt(3, booking_id);
                 preparedStatement.executeUpdate();
@@ -117,23 +118,43 @@ public class MethodsForMenu {
                     System.err.println("Database did not return generated booking ID");
                 } else {
                     int generatedCancellationId = generatedkey.getInt(1);
-                    System.out.println("Saved booking with booking ID " + generatedCancellationId);
+                    System.out.println("Saved Cancellation with cancellation_ID" + generatedCancellationId);
 
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        /*public void All_booking(){
-            System.out.println("****** All_Bookings *******");
+        public void All_booking(){
+            System.out.println("****** All_Bookings Detail *******");
             try (Connection connection = DriverManager.getConnection(databaseUrl, connectionProperties))
             {
                 Statement statement = connection.createStatement();
                 String sql;
                 // Query for display all students
-                sql = "
-        }*/
-    }
+                sql = "SELECT booking.Booking_ID, " +
+                        "booking.Check_in_Date, booking.Check_out_Date, " +
+                        "booking.Guest_ID,guest.NAME,guest.Surname,booking.Room_ID " +
+                        "FROM booking INNER JOIN guest on booking.Guest_ID = guest.GuestID";
+                ResultSet resultSet = statement.executeQuery(sql);
+                while (resultSet.next()){
+                    int booking_ID = resultSet.getInt("Booking_ID");
+                    Date checkin_Date = resultSet.getDate("Check_in_Date");
+                    Date check_outdate = resultSet.getDate("Check_out_Date");
+                    int guest_Id = resultSet.getInt("Guest_ID");
+                    String guest_name = resultSet.getString("NAME");
+                    String surname = resultSet.getString("Surname");
+                    int Room_ID = resultSet.getInt("Room_ID");
+                    System.out.println("Booking_Id: " +booking_ID +" |" +" Check_in: " +checkin_Date + " | "+" Check_out: " +check_outdate
+                            +" | " +" Guest_Id: " +guest_Id +" | "+" Booked by: " +guest_name +" | "+" Surname: " +surname +" | "+" Booked Room_Id: " +Room_ID);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        }
+
 
 
 
