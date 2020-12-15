db0b370b8cc558dee37170f62516088e2a2f50e1
Jenny Wong <yintingjenny.wong@gmail.com>
2020-12-15 11:48:55 +0100

    Redid merge between Git commits 220c8aeb and 7e5d2ad4

--- src/Menu.java
+++ src/Menu.java
@@ -1,24 +1,34 @@
-import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Menu {
-public void display_menu(){
-    System.out.println("1) Display all Available Rooms");
-    System.out.println("2) Display all Booked_Rooms");
-    System.out.println("3) Display all cancelled_Reservations");
-    System.out.println("4) Add new Reservation");
-    System.out.println("5) Cancel Reservation");
-    System.out.println("6) All_Guests Detail");
-    System.out.println("7) Exit");
-}
-    public void execute_menu(){
+
+    private final CancellationRepository cancellationRepository;
+    private final GuestRepo guestRepo;
+
+    public Menu(CancellationRepository cancellationRepository, GuestRepo guestRepo) {
+        this.cancellationRepository = cancellationRepository;
+        this.guestRepo = guestRepo;
+    }
+
+    public void display_menu() {
+        System.out.println("1) Display all Available Rooms");
+        System.out.println("2) Display all Booked_Rooms");
+        System.out.println("3) Display all cancelled_Reservations");
+        System.out.println("4) Add new Reservation");
+        System.out.println("5) Cancel Reservation");
+        System.out.println("6) All_Guests Detail");
+        System.out.println("7 Display all rooms");
+        System.out.println("8) Exit");
+    }
+
+    public void execute_menu() {
         int n = 0;
         try {
             do {
                 Scanner user_input = new Scanner(System.in);
                 System.out.println("Enter Your choice: ");
                 int x = user_input.nextInt();
-                if (x >= 0 && x <= 7) {
+                if (x >= 0 && x <= 8) {
                     switch (x) {
                         case 1: {
                             System.out.println("Display all Available Rooms");
@@ -29,35 +39,58 @@ public void display_menu(){
                             break;
                         }
                         case 3: {
-                            System.out.println("Display all cancelled_Reservations");
+                            displayAllCancelledReservations();
                             break;
                         }
                         case 4: {
-                            System.out.println("Add new Reservation");
+                            MethodsForMenu methodsForMenu = new MethodsForMenu();
+                            methodsForMenu.All_rooms();
+                            //need to add here method for avaiable rooms_Id
+                            methodsForMenu.Add_new_reservation();
+                            System.out.println("");
                             break;
                         }
                         case 5: {
                             System.out.println("Cancel Reservation");
                             break;
                         }
-                        case 6:{
-                            System.out.println("All_Guests Detail");
+                        case 6: {
+                            guestRepo.displayAllGuests();
                             break;
                         }
                         case 7: {
+                            MethodsForMenu methodsForMenu = new MethodsForMenu();
+                            methodsForMenu.All_rooms();
+                            break;
+                        }
+
+                        case 8: {
                             System.out.println("Exit");
-                            n =-1;
+                            n = -1;
                         }
 
                     }
                 } else {
                     System.out.println("Enter Valid number");
                 }
-            } while (n == 0);
+            } while (n > 0);
             System.out.println("End");
         } catch (NumberFormatException e) {
             System.out.println("Enter numeric value");
         }
     }
+
+    private void displayAllCancelledReservations() {
+        cancellationRepository.displayAllCancellation();
+    }
+
+    public void printHeader() {
+        System.out.println("*********************");
+        System.out.println("*** Welcome  ********");
+        System.out.println("***   to     ********");
+        System.out.println("*** Hotel System ***");
+        System.out.println("*********************");
+    }
+
 }
 
